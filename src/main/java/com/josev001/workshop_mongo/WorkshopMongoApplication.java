fb161eeabbe5836085c8fd3283acb4a77fd7836e
JoseV-001 <Leaphaz@gmail.com>
2026-02-06 21:33:57 -0300

    feat: integrate MongoDB with repository and service

--- src/main/java/com/josev001/workshop_mongo/WorkshopMongoApplication.java
+++ src/main/java/com/josev001/workshop_mongo/WorkshopMongoApplication.java
@@ -1,13 +1,23 @@
 package com.josev001.workshop_mongo;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WorkshopMongoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopMongoApplication.class, args);
+	}
+
+	@Bean
+	public MongoClientSettings mongoClientSettings() {
+		return MongoClientSettings.builder()
+				.applyConnectionString(new ConnectionString("mongodb://localhost:27017/workshop_mongo"))
+				.build();
 	}
 
 }
---
+++ src/main/java/com/josev001/workshop_mongo/service/UserService.java
@@ -0,0 +1,21 @@
+package com.josev001.workshop_mongo.service;
+
+import com.josev001.workshop_mongo.domain.User;
+import com.josev001.workshop_mongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+
+}
