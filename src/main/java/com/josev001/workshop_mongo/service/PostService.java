f555689c9a44eff992f15f0c4ae03482d2e7fbf0
JoseV-001 <Leaphaz@gmail.com>
2026-02-11 20:19:55 -0300

    feat: add endpoint to retrieve post by id

---
+++ src/main/java/com/josev001/workshop_mongo/service/PostService.java
@@ -0,0 +1,22 @@
+package com.josev001.workshop_mongo.service;
+
+import com.josev001.workshop_mongo.domain.Post;
+import com.josev001.workshop_mongo.repository.PostRepository;
+import com.josev001.workshop_mongo.service.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        return repo.findById(id)
+                .orElseThrow(() ->
+                        new ObjectNotFoundException("Objeto não encontrado")
+                );
+    }
+
+}
